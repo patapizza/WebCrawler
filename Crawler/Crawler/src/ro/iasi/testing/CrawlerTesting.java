6e010807e985117c1ec0bd10d917c50d7845bc05
haidelber <geo.haidelber@gmail.com>
2012-07-11 16:59:54 +0300

    libs

---
+++ Crawler/Crawler/src/ro/iasi/testing/CrawlerTesting.java
@@ -0,0 +1,16 @@
+package ro.iasi.testing;
+
+import ro.iasi.crawler.SimpleCrawler;
+
+public class CrawlerTesting {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SimpleCrawler crawler = new SimpleCrawler();
+		crawler.startCrawling();		
+
+	}
+
+}
