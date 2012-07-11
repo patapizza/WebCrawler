7d84d665223acb2639846e0b302099ad56e22997
Julien Odent <julien@odent.net>
2012-07-11 13:34:25 +0300

    updating assigner

--- Crawler/Assigner/src/Assigner.java
+++ Crawler/Assigner/src/Assigner.java
@@ -3,27 +3,32 @@ import java.util.PriorityQueue;
 
 public class Assigner {
 	
-	PriorityQueue<Node> domains;
+	private PriorityQueue<Node> domains;
 	
 	
-	public void Assigner() {
-		
+	public Assigner() {
+		this.domains = new PriorityQueue<Node>();
 		
 		
 	}
 	
-	/**
-	 * check the link and increment the reference counter 
-	 */
-	public void addLink(String domain, String url) {
+	public void addLinks(String domain, ArrayList<String> links) {
+		Node node = null;
 		for (node : domains)
-			if (node.getDomain().equals(domain)) {
-				if (node.hasURL(url))
-					return;
-				node.addURL(url);
+			if (node.getDomain().equals(domain))
 				break;
+		if (!node)
+			node = new Node(domain, domain);
+		for (String link : links)
+			// TODO: regex
+			if (link.substring(0, domain.length()).equals(domain))
+				node.addPage(link);
+			else {
+				Node toAdd = new Node(getDomain(link), link);
+				toAdd.addReferer(domain);
+				domains.add(toAdd);
 			}
-		domains.add(new Node(domain, url));
+		domains.add(node);
 	}
 	
 	/**
--- Crawler/Assigner/src/Node.java
+++ Crawler/Assigner/src/Node.java
@@ -21,21 +21,19 @@ public class Node {
 	 * timestamp of the current domain
 	 */
 	private Date timestamp;
-	
+
 	/**
 	 * 
 	 */
 	private ArrayList<String> referers;
-	
+
 	public Node(String domain, String url) {
-		
 		this.domain = domain;
 		this.pages = new ArrayList<String>();
 		this.pages.add(url);
+		this.referers = new ArrayList<String>();
+		this.referenceCounter = 0;
 		this.timestamp = new Date();
-		
-		
-		
 	}
 
 	public void updateTimestamp() {
@@ -44,6 +42,22 @@ public class Node {
 
 	public String getDomain() {
 		return domain;
+	}
+
+	public boolean hasPage(String url) {
+		return pages.contains(url);
+	}
+
+	public void addPage(String url) {
+		if (!pages.contains(url))
+			pages.add(url);
+	}
+
+	public void addReferer(String referer) {
+		if (referers.contains(referer))
+			return;
+		referers.add(referer);
+		++referenceCounter;
 	}
 	
 	
