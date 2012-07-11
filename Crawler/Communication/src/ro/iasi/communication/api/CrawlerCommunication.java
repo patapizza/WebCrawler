e90e4694f5263b32473a11a20000a8bcb1d83918
David <David@bnb>
2012-07-11 11:58:52 +0200

    crawler communication implementation

--- Crawler/Communication/src/ro/iasi/communication/api/CrawlerCommunication.java
+++ Crawler/Communication/src/ro/iasi/communication/api/CrawlerCommunication.java
@@ -1,8 +1,11 @@
 package ro.iasi.communication.api;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
+
 public interface CrawlerCommunication {
 	
-	public void sendLinks(LinksDTO linksDTO);
-	public LinksDTO getLinks();
+	public void sendLinks(LinksDTO linksDTO) throws UnknownHostException, IOException;
+	public LinksDTO getLinks() throws UnknownHostException, IOException, ClassNotFoundException;
 
 }
--- Crawler/Communication/src/ro/iasi/communication/impl/CrawlerCommunicationImpl.java
+++ Crawler/Communication/src/ro/iasi/communication/impl/CrawlerCommunicationImpl.java
@@ -1,20 +1,54 @@
 package ro.iasi.communication.impl;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 import ro.iasi.communication.api.CrawlerCommunication;
 import ro.iasi.communication.api.LinksDTO;
+import ro.iasi.communication.enums.Operation;
 
 public class CrawlerCommunicationImpl implements CrawlerCommunication {
+	
+	// TODO externalize
+	public static final String IP = "127.0.0.1";
+	public static final int PORT = 6666;
 
 	@Override
-	public LinksDTO getLinks() {
-		// TODO Auto-generated method stub
-		return null;
+	public LinksDTO getLinks() throws UnknownHostException, IOException, ClassNotFoundException {
+		Socket socket = createSocketForOperation(Operation.GET);
+		
+		ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+		LinksDTO linksDTO = (LinksDTO) objectInputStream.readObject();
+		
+		objectInputStream.close();
+		socket.close();
+		
+		return linksDTO;
 	}
 
 	@Override
-	public void sendLinks(LinksDTO linksDTO) {
-		// TODO Auto-generated method stub
+	public void sendLinks(LinksDTO linksDTO) throws UnknownHostException, IOException {
+		Socket socket = createSocketForOperation(Operation.SEND);
 		
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		objectOutputStream.writeObject(linksDTO);
+		
+		objectOutputStream.close();
+		socket.close();
+	}
+	
+	private Socket createSocketForOperation(Operation operation) throws UnknownHostException, IOException {
+		Socket socket = new Socket(InetAddress.getByName(CrawlerCommunicationImpl.IP), CrawlerCommunicationImpl.PORT);
+		
+		ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+		objectOutputStream.writeObject(operation);
+		objectOutputStream.close();
+		
+		return socket;
 	}
 
 }
