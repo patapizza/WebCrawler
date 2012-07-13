ef0ca31085059733f464a08c33ce790a343d6666
David <David@bnb>
2012-07-13 10:39:56 +0200

    storing words into database

--- Crawler/DBManager/src/ro/iasi/db/test/TestMain.java
+++ Crawler/DBManager/src/ro/iasi/db/test/TestMain.java
@@ -2,6 +2,9 @@ package ro.iasi.db.test;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
 import ro.iasi.communication.db.api.DBCrawlerCommunication;
 import ro.iasi.communication.db.impl.DBCrawlerCommunicationImpl;
@@ -10,7 +13,12 @@ public class TestMain {
 
 	public static void main(String[] args) throws UnknownHostException, ClassNotFoundException, IOException {
 		DBCrawlerCommunication communication = new DBCrawlerCommunicationImpl();
-		System.out.println(communication.getDictionary().size());
+		
+		Map<String, Integer> dictionary = communication.getDictionary();
+		System.out.println(dictionary.size());
+		
+		Map<String, Map<String, List<Integer>>> indexes = new LinkedHashMap<>();
+//		List<Integer> wordIds = 
 	}
 
 }
