fade4b63fa8b1e29feeaac56c31de5bf781b9ceb
David <David@bnb>
2012-07-13 11:23:38 +0200

    db manager test

--- Crawler/DBManager/src/ro/iasi/db/test/TestMain.java
+++ Crawler/DBManager/src/ro/iasi/db/test/TestMain.java
@@ -2,11 +2,13 @@ package ro.iasi.db.test;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import ro.iasi.communication.db.api.DBCrawlerCommunication;
+import ro.iasi.communication.db.api.IndexesDTO;
 import ro.iasi.communication.db.impl.DBCrawlerCommunicationImpl;
 
 public class TestMain {
@@ -18,7 +20,16 @@ public class TestMain {
 		System.out.println(dictionary.size());
 		
 		Map<String, Map<String, List<Integer>>> indexes = new LinkedHashMap<>();
-//		List<Integer> wordIds = 
+		List<Integer> wordIds = new ArrayList<>();
+		wordIds.add(dictionary.get("dog"));
+		Map<String, List<Integer>> urls = new LinkedHashMap<>();
+		urls.put("google.com/assadasd", wordIds);
+		indexes.put("google.com", urls);
+		
+		IndexesDTO indexesDTO = new IndexesDTO();
+		indexesDTO.setIndexes(indexes);
+		
+		communication.pushIndexes(indexesDTO);
 	}
 
 }
