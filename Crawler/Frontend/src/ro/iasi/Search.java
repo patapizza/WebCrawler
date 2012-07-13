c0b825ba0005d50599122c7176a1cac6847e98c1
Standa Jasa <standa@jasa.name>
2012-07-13 12:23:45 +0300

    adsf

--- Crawler/Frontend/src/ro/iasi/Search.java
+++ Crawler/Frontend/src/ro/iasi/Search.java
@@ -1,6 +1,8 @@
 package ro.iasi;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -21,10 +23,35 @@ public class Search extends HttpServlet {
     }
 
 	/**
+	 * Fetch the data from database using the cosine similarity algorhitm
+	 * 
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
+				
+ 
+		    response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+
+	        out.println("<html>");
+	        out.println("<head>");
+
+		    out.println("<title>" + "</title>");
+	        out.println("</head>");
+	        out.println("<body bgcolor=\"white\">");
+
+
+		    out.println("<a href=\"../helloworld.html\">");
+	        out.println("<img src=\"../images/code.gif\" height=24 " +
+	                    "width=24 align=right border=0 alt=\"view code\"></a>");
+	        out.println("<a href=\"../index.html\">");
+	        out.println("<img src=\"../images/return.gif\" height=24 " +
+	                    "width=24 align=right border=0 alt=\"return\"></a>");
+	        out.println("<h1>" + "</h1>");
+	        out.println("</body>");
+	        out.println("</html>");		
+		
 	}
 
 	/**
@@ -35,52 +62,3 @@ public class Search extends HttpServlet {
 	}
 
 }
-
-/**
- 
-
-public class HelloWorldExample extends HttpServlet {
-
-
-    public void doGet(HttpServletRequest request,
-                      HttpServletResponse response)
-        throws IOException, ServletException
-    {
-        ResourceBundle rb =
-            ResourceBundle.getBundle("LocalStrings",request.getLocale());
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-
-        out.println("<html>");
-        out.println("<head>");
-
-	    String title = rb.getString("helloworld.title");
-
-	    out.println("<title>" + title + "</title>");
-        out.println("</head>");
-        out.println("<body bgcolor=\"white\">");
-
-	// note that all links are created to be relative. this
-	// ensures that we can move the web application that this
-	// servlet belongs to to a different place in the url
-	// tree and not have any harmful side effects.
-
-        // XXX
-        // making these absolute till we work out the
-        // addition of a PathInfo issue
-
-	    out.println("<a href=\"../helloworld.html\">");
-        out.println("<img src=\"../images/code.gif\" height=24 " +
-                    "width=24 align=right border=0 alt=\"view code\"></a>");
-        out.println("<a href=\"../index.html\">");
-        out.println("<img src=\"../images/return.gif\" height=24 " +
-                    "width=24 align=right border=0 alt=\"return\"></a>");
-        out.println("<h1>" + title + "</h1>");
-        out.println("</body>");
-        out.println("</html>");
-    }
-}
-
-
-
-*/
