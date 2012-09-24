ce678e474b3682d4e4bd8668f54d3f4f562a4d31
patapizza <patapizza@gmail.com>
2012-09-24 18:37:39 +0200

    finalizing parsing

--- crawler/parser.py
+++ crawler/parser.py
@@ -8,23 +8,24 @@ class Parser:
     def __init__(self, words, url):
         self.words = words
 	self.url = url
-	self.html = self.fetch_url()
 	self.wc = []
 	self.links = []
+	self.preprocess()
 
-    # return the words counts from document
+    # retrieve words and links from document, count the word occurrences
+    def preprocess(self):
+        html = self.fetch_url()
+        tokens = filter(lambda(x): x, re.split('\W+', re.sub(r'<[^>]+>', '', html)))
+	self.links = map(lambda(x): re.sub(r'.*(?<!href=[\'"])href=[\'"]([^\'"]+)[\'"].*', '\\1', x), re.findall(r'<a [^>]+>[^<]+</a>', html))
+	self.wc = [tokens.count(w) for w in self.words]
+
+    # return the word counts from document
     def get_wc(self):
         return self.wc
 
     # return the links from document
     def get_links(self):
         return self.links
-
-    # retrieve words and links in document
-    def parse(self):
-        self.words = filter(lambda(x): x, re.split('\W+', re.sub(r'<[^>]+>', '', self.html)))
-	self.links = re.findall(r'<a [^>]+>[^<]+</a>', self.html)
-	self.links = map(lambda(x): re.sub(r'.*(?<!href=[\'"])href=[\'"]([^\'"]+)[\'"].*', '\\1', x), self.links)
 
     # retrieve the document
     def fetch_url(self):
@@ -34,6 +35,6 @@ class Parser:
 	return html
 
 if __name__ == '__main__':
-    p = Parser([], "http://www.google.com")
-    p.parse()
+    p = Parser(["google", "a"], "http://www.google.com")
     print(p.get_links())
+    print(p.get_wc())
