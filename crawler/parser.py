3adeb671d8434d2d476f4dd6fece21f239babee5
patapizza <patapizza@gmail.com>
2012-10-11 11:46:21 +0200

    fixing tab/space issue

--- crawler/parser.py
+++ crawler/parser.py
@@ -7,18 +7,18 @@ class Parser:
 
     def __init__(self, words):
         self.words = words
-	self.wc = []
-	self.links = []
-	self.url = None
+        self.wc = []
+        self.links = []
+        self.url = None
 
     # retrieve words and links from document, count the word occurrences
     def preprocess(self):
         if not self.url:
-	    return False
+            return False
         html = self.fetch_url()
         tokens = filter(lambda(x): x, re.split('\W+', re.sub(r'<[^>]+>', '', html)))
-	self.links = map(lambda(x): re.sub(r'.*(?<!href=[\'"])href=[\'"]([^\'"]+)[\'"].*', '\\1', x), re.findall(r'<a [^>]+>[^<]+</a>', html))
-	self.wc = [tokens.count(w) for w in self.words]
+        self.links = map(lambda(x): re.sub(r'.*(?<!href=[\'"])href=[\'"]([^\'"]+)[\'"].*', '\\1', x), re.findall(r'<a [^>]+>[^<]+</a>', html))
+        self.wc = [tokens.count(w) for w in self.words]
 
     # return the word counts from document
     def get_wc(self):
@@ -31,11 +31,11 @@ class Parser:
     # retrieve the document
     def fetch_url(self):
         sock = urllib2.urlopen(self.url)
-	html = sock.read()
-	sock.close()
-	return html
+        html = sock.read()
+        sock.close()
+        return html
 
     # feed in a new url to parse
     def set_url(self, url):
         self.url = url
-	self.preprocess()
+        self.preprocess()
--- crawler/pnode.py
+++ crawler/pnode.py
@@ -7,9 +7,9 @@ class PNode:
 
     def __init__(self, domain, page):
         self.domain = domain
-	self.pages = [Page(page)]
-	self.referers = []
-	self.timestamp = time()
+        self.pages = [Page(page)]
+        self.referers = []
+        self.timestamp = time()
 
     def get_domain(self):
         return self.domain
@@ -31,7 +31,7 @@ class PNode:
     def add_referer(self, referer):
         referers = [r.get_domain() for r in self.referers]
         if not referer.get_domain() in set(referers):
-	    self.referers.append(referer)
+            self.referers.append(referer)
 
     def update_timestamp(self):
         self.timestamp = time()
