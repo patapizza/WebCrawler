f73ad804b5125ef84dabb252768990501d6059a3
Michael Saint-Guillain <stguillain@virunga.info.ucl.ac.be>
2012-10-11 14:13:09 +0200

    blabl

--- assigner/domains_manager.py
+++ assigner/domains_manager.py
@@ -11,28 +11,34 @@ class DomManager :
     
     def update_domain(self, domain, update_timestamp=True) :
         if domain.get_domain() in  self.domains.keys() : # if domain already in dict
-            curr_domain = self.domains[domain.get_domain()]
+            curr_domain, _ = self.domains[domain.get_domain()]
             for page in domain.get_pages() :
                 curr_domain.add_page(page) 
             for ref in domain.get_referers() :
                 curr_domain.add_referer(ref)
             if update_timestamp : curr_domain.update_timestamp()
+            self.domains[domain.get_domain()] = [domain, False] # domain is not new anymore
         else  :                                         # if domain not in dict
-            self.domains[domain.get_domain()] = [domain, ]
+            self.domains[domain.get_domain()] = [domain, True]
         
     def update_externals(self, externals)
         for domain in externals :
-            update_domain(domain, False)
+            update_domain(domain, update_timestamp=False)
     
     
     def select_domain() :
         oldest = time()
+        isnew = False
         domain = None
-        for dom, new in domains.values()
+        for dom, new in self.domains.values()
             if new : 
                 domain = dom
+                isnew = True
                 break
             if dom.get_timestamp() < oldest :
                 oldest = dom.get_timestamp()
                 domain = dom
+        if not isnew :
+            domain.update_timestamp()        
+        return domain
         
