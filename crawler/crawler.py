0eaf40c9283cedfdca0e95fe14785dc4c83ccb7a
patapizza <patapizza@gmail.com>
2012-10-11 11:41:06 +0200

    fixing tab/space issue

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -8,37 +8,37 @@ class Crawler:
 
     def __init__(self, domains, words):
         self.internals = [PNode(domain, "/") for domain in domains]
-	self.words = words
-	self.externals = []
+	    self.words = words
+	    self.externals = []
     
     def crawl(self):
         p = Parser(self.words)
         for pnode in self.internals:
-	   for page in pnode.get_pages():
-	       p.set_url(''.join([pnode.get_domain(), page.get_name()]))
-	       page.set_wc(p.get_wc())
-	       links = p.get_links()
-	       page.set_links(links)
-	       self.add_links(pnode, links)
+	        for page in pnode.get_pages():
+	            p.set_url(''.join([pnode.get_domain(), page.get_name()]))
+	            page.set_wc(p.get_wc())
+	            links = p.get_links()
+	            page.set_links(links)
+	            self.add_links(pnode, links)
 
     def add_links(self, pointer, links):
-	for link in links:
-	    domain = extract_domain(link)
-	    ptr = None
-	    for internal in self.internals:
-	        if domain == internal.get_domain():
-	            internal.add_page(link)
-		    ptr = internal
-	    if not ptr:
-	        pnode = None
-	        for external in self.externals:
-		    if external.get_domain() == domain:
-		        pnode = external
-			pnode.add_page(link)
-		if not pnode:
-	            pnode = PNode(domain, link)
-		    self.externals.append(pnode)
-		pnode.add_referer(pointer)
+	    for link in links:
+	        domain = extract_domain(link)
+	        ptr = None
+	        for internal in self.internals:
+	            if domain == internal.get_domain():
+	                internal.add_page(link)
+		            ptr = internal
+	        if not ptr:
+	            pnode = None
+	            for external in self.externals:
+		            if external.get_domain() == domain:
+		                pnode = external
+			            pnode.add_page(link)
+		        if not pnode:
+	                pnode = PNode(domain, link)
+		            self.externals.append(pnode)
+		        pnode.add_referer(pointer)
     
     def get_externals(self):
         return self.externals
